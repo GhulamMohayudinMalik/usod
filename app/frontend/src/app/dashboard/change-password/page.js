f6389efb8320ef7e710ba92da3027a04bd89641b
Ghulam Mohayudin <ghulammohayudinmalik@gmail.com>
2025-11-26 14:55:08 +0500

    Fixing apos error

--- app/frontend/src/app/dashboard/change-password/page.js
+++ app/frontend/src/app/dashboard/change-password/page.js
@@ -240,7 +240,7 @@ export default function ChangePasswordPage() {
               <svg className="w-4 h-4 mr-2 mt-0.5 text-blue-400" fill="currentColor" viewBox="0 0 20 20">
                 <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
               </svg>
-              Don't reuse passwords from other accounts
+              Don&apos;t reuse passwords from other accounts
             </li>
             <li className="flex items-start">
               <svg className="w-4 h-4 mr-2 mt-0.5 text-blue-400" fill="currentColor" viewBox="0 0 20 20">
